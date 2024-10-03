5f6b21c35d49c75546e5e5c4c99da01134254c44
Jev <jev.kuznetsov@gmail.com>
2024-10-03 11:51:06 +0000

    update tests

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -3,12 +3,24 @@ import pytest
 
 from miltonmail import crypto
 
+PASS = "mysecretpassphrase"
+
 
 def test_phrase() -> None:
     with pytest.raises(ValueError):
         crypto.get_passphrase()
 
     # set the passphrase
-    os.environ["MILTON_PASS"] = "mysecretpassphrase"
+    os.environ["MILTON_PASS"] = PASS
 
-    assert crypto.get_passphrase() == "mysecretpassphrase"
+    assert crypto.get_passphrase() == PASS
+
+
+def test_encrypt_decrypt() -> None:
+    os.environ["MILTON_PASS"] = PASS
+
+    password = "mysecretpassword"
+    encrypted_password, salt = crypto.encrypt_password(password, PASS)
+
+    assert password != encrypted_password
+    assert password == crypto.decrypt_password(encrypted_password, PASS, salt)
